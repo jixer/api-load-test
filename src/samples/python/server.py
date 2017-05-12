8e058d09a3938028a193e65b4ee5eed82c539316
Chris Myers <gsxr.signup@gmail.com>
2017-05-12 11:23:48 -0700

    Simple Python HTTP listener

---
+++ src/samples/python/server.py
@@ -0,0 +1,21 @@
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+
+class HTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write("Hello world")
+        return
+
+def main():
+    print "Starting HTTP listener on port 8080"
+    server = HTTPServer(('', 8080), HTTPRequestHandler)
+
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print '^C received, shutting down the web server'
+        server.socket.close()
+
+if __name__=="__main__":
+    main()
