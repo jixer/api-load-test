5fa008c050834b22857956fd042adf953cef7949
Chris' Mac <chris@MacBook.local>
2017-05-11 22:12:06 -0700

    ASP.NET Core Web API

---
+++ src/samples/netcore/Controllers/ValuesController.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPIApplication.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello world";
+        }
+    }
+}
